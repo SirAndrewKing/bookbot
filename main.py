05f13abac0094b419b0231b3d326fc042f61e9e1
SirAndrewKing <sirandrewking.git@gmail.com>
2024-03-30 11:53:13 -0500

    added lower_text (bad name) to count occurence of letters, etc.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
   num_words = get_num_words(text)
+  lower_text= get_num_letters(text)
   print(f"{num_words} words found in the document")
+  print(f"{lower_text}")
 
 def get_num_words(text):
   words = text.split()
@@ -11,5 +13,17 @@ def get_num_words(text):
 def get_book_text(path):
   with open(path) as f:
     return f.read()
+  
+def get_num_letters(text):
+  chars = {}
+  for c in text:
+    lowered = c.lower()
+    if lowered in chars:
+      chars[lowered] +=1
+    else:
+      chars[lowered] = 1
+  return chars
+
+
 
 main()
